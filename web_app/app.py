f8121c780334a5f80e8afb31260c830f3d972089
jbell1991 <joseph.louis.bell@gmail.com>
2020-02-24 23:24:39 -0500

    Starting to organize app structure, render an HTML page

---
+++ web_app/app.py
@@ -0,0 +1,34 @@
+from flask import Flask, jsonify, render_template
+
+
+def create_app():
+
+    app = Flask(__name__)
+
+    @app.route("/")
+    def hello():
+        return "Hello World!"
+
+    @app.route("/about")
+    def about():
+        return "About me"
+
+    @app.route("/books.json")
+    def list_books():
+        books = [
+            {"id": 1, "title": "Book 1"},
+            {"id": 2, "title": "Book 2"},
+            {"id": 3, "title": "Book 3"}
+        ]
+        return jsonify(books)
+
+    @app.route("/books")
+    def list_books_for_humans():
+        books = [
+            {"id": 1, "title": "Book 1"},
+            {"id": 2, "title": "Book 2"},
+            {"id": 3, "title": "Book 3"}
+        ]
+        return render_template("books.html")
+
+    return app
